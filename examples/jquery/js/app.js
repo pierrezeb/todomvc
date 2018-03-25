e24dda507970dba20841b6b695fffba5ea78fa85
pierrezeb <36326642+pierrezeb@users.noreply.github.com>
2018-03-25 14:09:03 +0200

    Update app.js

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -70,7 +70,7 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
+			
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -90,7 +90,7 @@ jQuery(function ($) {
 			this.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
-
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +117,7 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -147,12 +148,13 @@ jQuery(function ($) {
 			});
 
 			$input.val('');
-
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		edit: function (e) {
@@ -188,6 +190,7 @@ jQuery(function ($) {
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			util.store('todos-jquery', this.todos);
 			this.render();
 		}
 	};
